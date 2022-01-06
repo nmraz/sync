6dd953caef40afbb96f6b1ae301d1801e7922621
Noam Raz <noamraz8@gmail.com>
2022-01-06 19:03:38 +0200

    Make mutex non-copyable

--- sync/include/sync/mutex.h
+++ sync/include/sync/mutex.h
@@ -7,6 +7,12 @@ class Mutex {
 public:
     constexpr Mutex() = default;
 
+    Mutex(const Mutex&) = delete;
+    Mutex(Mutex&&) = delete;
+
+    Mutex& operator=(const Mutex&) = delete;
+    Mutex& operator=(Mutex&&) = delete;
+
     void lock();
     void unlock();
 
