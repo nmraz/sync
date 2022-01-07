720b290671862f26cf35f2781d35e1cb78f27ad1
Noam Raz <noamraz8@gmail.com>
2022-01-07 12:40:36 +0200

    Reorganize directory structure

--- src/bin/main.cpp
+++ src/bin/main.cpp
@@ -1,10 +1,14 @@
 #include <sync/mutex.h>
+
 #include <functional>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
 
-void inc_many(int& val, syncobj::Mutex& mutex) {
+using MyMutex = syncobj::Mutex;
+
+void inc_many(int& val, MyMutex& mutex) {
     for (int i = 0; i < 1000000; i++) {
         mutex.lock();
         val++;
@@ -13,12 +17,12 @@ void inc_many(int& val, syncobj::Mutex& mutex) {
 }
 
 int main() {
-    syncobj::Mutex mutex;
+    MyMutex mutex;
     int val = 0;
 
     std::vector<std::thread> threads;
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 8; i++) {
         threads.emplace_back(inc_many, std::ref(val), std::ref(mutex));
     }
 
